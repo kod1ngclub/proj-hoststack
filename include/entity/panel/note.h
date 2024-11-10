ccbf4a4c62a8babbccb30dfd280e29812a14162f
kod1ngclub <wiiwefwefbi@gmail.com>
2024-11-10 21:21:42 +0900

    CHORE: Remove include directory

--- include/entity/panel/note.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef const char* const NoteId;
-typedef const char* const NoteContent;
-
-typedef struct {
-    NoteId id;
-    NoteContent content;
-} Note;
--- include/entity/rule/role.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef enum {
-    ROLE_GUEST,
-    ROLE_MEMBER,
-    ROLE_ADMIN
-} Role;
