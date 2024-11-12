b188300501be37e0ad801632b642ae8fe3e2c44f
kod1ngclub <wiiwefwefbi@gmail.com>
2024-11-12 19:21:55 +0900

    FEAT: Make bool.h in service cuz it uses 'Bool' in entity but it was accidental duplication

---
+++ src/include/service/shared/bool.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "stdbool.h"
+
+typedef bool Bool;
+
+// It's same with entity/shared/bool.h
+// But, they're an accidental duplication
+// => So must define them independently
