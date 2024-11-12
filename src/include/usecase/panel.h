93c53c7f703c7b0571fa4ae430d2feea4bed4664
kod1ngclub <wiiwefwefbi@gmail.com>
2024-11-12 18:51:38 +0900

    FEAT: Apply store service to every usecase

--- src/include/usecase/panel.h
+++ src/include/usecase/panel.h
@@ -3,7 +3,11 @@
 // entity
 #include "../entity/panel/panel.h"
 
+// service
+#include "../service/store.h"
+
 // usecase error model
+#include "shared/dep.h"
 #include "shared/error.h"
 
 // REF define util
@@ -11,22 +15,29 @@
 
 // usecase functions
 UsecaseResult ReadPanel(
+    DEP(Store, panelstore),
+
     REF(Panel, panel)
 );
 
 UsecaseResult ReadNote(
+    DEP(Store, notestore),
     NoteId id,
 
     REF(Note, note)
 );
 
 UsecaseResult CreateNote(
+    DEP(Store, panelstore),
+    DEP(Store, notestore),
     NoteContent content,
 
     REF(Note, note)
 );
 
 UsecaseResult DeprecateNote(
+    DEP(Store, panelstore),
+    DEP(Store, notestore),
     NoteId id,
 
     REF(Note, note)
--- src/include/usecase/rule.h
+++ src/include/usecase/rule.h
@@ -5,28 +5,38 @@
 #include "../entity/rule/role.h"
 #include "../entity/rule/user.h"
 
+// service
+#include "../service/store.h"
+
 // usecase error model
 #include "shared/error.h"
+#include "shared/dep.h"
 
 // REF define util
 #include "shared/ref.h"
 
 // usecase functions
 UsecaseResult ReadConfig(
+    DEP(Store, configstore),
+    
     REF(Config, config)
 );
 
 UsecaseResult ReadUsers(
+    DEP(Store, userstore),
+
     REF_LIST(User, users)
 );
 
 UsecaseResult KickUser(
+    DEP(Store, userstore),
     UserId id,
 
     REF(User, user)
 );
 
 UsecaseResult BanUser(
+    DEP(Store, userstore),
     UserId id,
 
     REF(User, user)
