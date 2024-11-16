f3dbda597e5c5c61989c1e85b19a71a356920ac3
kod1ngclub <wiiwefwefbi@gmail.com>
2024-11-16 15:37:02 +0900

    FEAT: Add config 'READ_STACKED'

--- src/include/entity/rule/config.h
+++ src/include/entity/rule/config.h
@@ -2,19 +2,22 @@
 
 #include "../shared/bool.h"
 
-typedef struct {    
+typedef struct {
     // item management
     const Bool GUEST_READ_ITEM;
     const Bool GUEST_CREATE_ITEM;
     const Bool GUEST_DEPRECATE_ITEM;
+    const Bool GUEST_READ_STACKED;
 
     const Bool MEMBER_READ_ITEM;
     const Bool MEMBER_CREATE_ITEM;
     const Bool MEMBER_DEPRECATE_ITEM;
+    const Bool MEMBER_READ_STACKED;
 
     const Bool ADMIN_READ_ITEM;
     const Bool ADMIN_CREATE_ITEM;
     const Bool ADMIN_DEPRECATE_ITEM;
+    const Bool ADMIN_READ_STACKED;
 
     // admin server management
     const Bool ADMIN_BAN;
